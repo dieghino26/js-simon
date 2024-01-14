15bf3ba983848488babe46baa3674d98eb78661a
dieghino26 <brotzadiego@gmail.com>
2024-01-14 16:14:45 +0100

    added random numbers

--- js/simon.js
+++ js/simon.js
@@ -1,3 +1,21 @@
 // prendere gli elementi della pagina
-const timer = document.getElementById("timer")
-const numbers = document.getElementById("numbers")
+const timer = document.getElementById("timer");
+const numbers = document.getElementById("numbers");
+const button = document.getElementById("button");
+
+
+button.addEventListener("click", function() {
+    
+    const randomNumbers = [];
+    while(randomNumbers.length < 5){
+        const randomNumber = Math.floor(Math.random () * 100) + 1;
+        randomNumbers.push(randomNumber);
+        console.log(randomNumbers);
+    }
+    button.classList.add("d-none")
+
+    numbers.innerHTML = randomNumbers
+});
+
+
+
